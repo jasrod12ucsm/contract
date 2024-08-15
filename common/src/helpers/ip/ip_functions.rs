f7b9c9cd791eb1d3da01b4feed4b3ef8adc7a2f5
jasrod12ucsm <jason.rodriguez@ucsm.edu.pe>
2024-08-15 13:09:48 -0500

    ip

--- common/src/helpers/ip/ip_functions.rs
+++ common/src/helpers/ip/ip_functions.rs
@@ -12,7 +12,7 @@ impl IpFunctions {
     pub fn get_local_ipv4() -> Result<String, Box<dyn std::error::Error>> {
         let interfaces = get_if_addrs().unwrap();
         for iface in interfaces {
-            if iface.name == "wlan0" {
+            if iface.name.starts_with("w") {
                 if let IpAddr::V4(ipv4) = iface.addr.ip() {
                     if ipv4 != std::net::Ipv4Addr::LOCALHOST {
                         return Ok(ipv4.to_string());
